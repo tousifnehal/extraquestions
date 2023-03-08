0b9eb65c6fbe2d8d09db3e839f2f083b17759943
Caution_Gamer <tousifnehalpratno06@gmail.com>
2023-03-08 23:19:52 +0600

    V 2.0.0

--- js/index.js
+++ js/index.js
@@ -41,35 +41,5 @@ const navSlide = () => {
   
   navSlide();
 
-  function search() {
-    const list = document.getElementById('list');
-const number = document.getElementById('number');
-var url = `https://panjeree.com/c09/${list.value}q${number.value}.pdf`
-  // Get the entered number
-  const enteredNumber = number.value;
 
-  // Check if the entered number is 4 digits long
-  if (enteredNumber.length === 4) {
-    // Redirect to the URL with the selected list value and entered number
-    window.open(window.location.href =url, '_blank');
-  } else if(enteredNumber.length > 4) {
-    // Display an error message if the entered number is not 4 digits long
-    alert('Please enter a 4-digit number.');
-  } else if(enteredNumber.length < 4) {
- alert('Please enter a 4-digit number.');
-  };
-  }
-
-  function showSelected() {
-    var select = document.getElementById("list");
-    var selectedOption = select.options[select.selectedIndex].value;
-  
-    if (selectedOption == "") {
-      alert("You Must Select One")
-    } else {
-      var input = document.getElementById("number")
-      select.options[0].disabled = true;
-      input.disabled = false;
-    }
-  }
-
+ 
