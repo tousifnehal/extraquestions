32aea9f249f64be7d3f40c5ac6c938348ad511d3
Caution_Gamer <tousifnehalpratno06@gmail.com>
2023-03-09 21:38:26 +0600

    V 2.0.0

--- js/index.js
+++ js/index.js
@@ -18,37 +18,34 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-  
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
-  
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
-      });
-  
-      burger.classList.toggle('toggle');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
     });
-  };
-  
-  navSlide();
 
-  function showSelected() {
-    var select = document.getElementById("mainDropdown");
-    var selectedOption = select.options[select.selectedIndex].value;
-    if (selectedOption !== "") {
-      select.options[0].disabled = true;
-    }
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
+
+function showSelected() {
+  var select = document.getElementById("mainDropdown");
+  var selectedOption = select.options[select.selectedIndex].value;
+  if (selectedOption !== "") {
+    select.options[0].disabled = true;
   }
-
-
-
- 
+}
